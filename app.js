1cb93367ff6f7650d52461d945d8e23bb0576de4
Tim Reilly <timreilly@live.com>
2017-09-06 17:17:16 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var url = require('url');
+var request = require('request');
+var express = require('express'); 
+
+var options = {
+  protocol: "http:",
+  host: "search.twitter.com",
+  pathname: '/search.json',
+  query: {
+    q: "codeschool"
+  }
+};
+
+var searchURL = url.format(options);
+
+var app = express(); // Create server here
+
+app.get('/', function(req, res){
+  request(searchURL).pipe(res); 
+}); 
+
+app.listen(8080); 
